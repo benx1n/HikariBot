0c6c3bee19d5c01b245f20c266436e22b256976b
Wangbo <ppqqows@hotmail.com>
2022-07-01 18:02:30 +0800

    Add github action auto build

--- 启动.bat
+++ 启动.bat
@@ -1,6 +1,22 @@
 @echo off
+if exist %~dp0\pyenv (
+    set PATH=%~dp0pyenv\Library\bin;%~dp0pyenv
+    set PLAYWRIGHT_BROWSERS_PATH=0
+)
+
+cd /d %~dp0
+if not exist .env.prod (
+    goto nofile
+)
+
+:start
 echo try to launch the HikariBot
 start http://127.0.0.1:8080/go-cqhttp/
-cd /d %~dp0
-nb run
+python -m nb_cli run
+pause
+goto:eof
+
+:nofile
+echo Please prepare a .env.prod file first!
+echo Exiting..
 pause
