d005077923414ec8877202389e5f16114df75540
benx1n <1119809439@qq.com>
2022-07-10 04:22:32 +0800

    忘记改了）

--- src/plugins/hikari_bot/wws_info.py
+++ src/plugins/hikari_bot/wws_info.py
@@ -60,7 +60,7 @@ async def get_AccountInfo(server_type,qqid,info):
                 return '您似乎准备用游戏昵称查询，请检查参数中是否包含服务器和游戏昵称，以空格区分，如果您准备查询单船战绩，请带上ship参数'
         else:
             return '参数似乎出了问题呢'
-        url = 'https://api.wows.shinoaki.com/public/wows/account/v3/user/info'
+        url = 'https://api.wows.shinoaki.com/public/wows/account/v4/user/info'
         logger.info(f"下面是本次请求的参数，如果遇到了问题，请将这部分连同报错日志一起发送给麻麻哦\n{url}\n{params}")
         async with httpx.AsyncClient(headers=headers) as client:
             resp = await client.get(url, params=params, timeout=None)
