253d84ac576b5f269a3f242fd975fd4a38c4922b
benx1n <1119809439@qq.com>
2022-07-18 22:51:39 +0800

    修改接口url

--- src/plugins/hikari_bot/wws_ship.py
+++ src/plugins/hikari_bot/wws_ship.py
@@ -100,7 +100,7 @@ async def get_ShipInfo(server_type,qqid,info,bot):
                 return '找不到船'
         else:
             return '参数似乎出了问题呢'
-        url = 'https://api.wows.shinoaki.com/public/wows/account/v2/ship/info'
+        url = 'https://api.wows.shinoaki.com/public/wows/account/ship/info'
         logger.info(f"下面是本次请求的参数，如果遇到了问题，请将这部分连同报错日志一起发送给麻麻哦\n{url}\n{params}")
         ranking = await get_MyShipRank_yuyuko(params)
         async with httpx.AsyncClient(headers=headers) as client:
