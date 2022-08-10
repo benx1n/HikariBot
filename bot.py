3a67fea49215a3bf74b85cf24ac37439f666aa68
benx1n <1119809439@qq.com>
2022-08-10 19:49:49 +0800

    修复创建子线程导致的log失效问题

--- bot.py
+++ bot.py
@@ -6,34 +6,6 @@ from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
 from nonebot.log import logger,default_format
 from pathlib import Path
 
-logger.add(
-    "logs/error.log",
-    rotation="00:00",
-    retention="1 week",
-    diagnose=False,
-    level="ERROR",
-    format=default_format,
-    encoding="utf-8",
-)
-logger.add(
-    "logs/info.log",
-    rotation="00:00",
-    retention="1 week",
-    diagnose=False,
-    level="INFO",
-    format=default_format,
-    encoding="utf-8",
-)
-logger.add(
-    "logs/warning.log",
-    rotation="00:00",
-    retention="1 week",
-    diagnose=False,
-    level="WARNING",
-    format=default_format,
-    encoding="utf-8",
-)
-
 nonebot.init()
 app = nonebot.get_asgi()
 
@@ -44,6 +16,33 @@ config.nb2_path = Path(__file__).parent
 
 
 if __name__ == "__mp_main__" or ("run" in sys.argv and "nb" in sys.argv[0]): 
+    logger.add(
+        "logs/error.log",
+        rotation="00:00",
+        retention="1 week",
+        diagnose=False,
+        level="ERROR",
+        format=default_format,
+        encoding="utf-8",
+    )
+    logger.add(
+        "logs/info.log",
+        rotation="00:00",
+        retention="1 week",
+        diagnose=False,
+        level="INFO",
+        format=default_format,
+        encoding="utf-8",
+    )
+    logger.add(
+        "logs/warning.log",
+        rotation="00:00",
+        retention="1 week",
+        diagnose=False,
+        level="WARNING",
+        format=default_format,
+        encoding="utf-8",
+    )
     if driver.config.use_plugin_go_cqhttp:
         nonebot.load_plugin('nonebot_plugin_gocqhttp')
     nonebot.load_from_toml("pyproject.toml")
