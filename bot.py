0a86020e70f00b9f4b0f2387e619cead3ac8f133
benx1n <1119809439@qq.com>
2023-11-15 18:21:16 +0800

    [🚀]update 1.0.3

--- bot.py
+++ bot.py
@@ -16,40 +16,40 @@ config = nonebot.get_driver().config
 config.nb2_path = Path(__file__).parent
 
 
-if __name__ == "__mp_main__" or ("run" in sys.argv and "nb" in sys.argv[0]):
+if __name__ == '__mp_main__' or ('run' in sys.argv and 'nb' in sys.argv[0]):
     logger.add(
-        "logs/error.log",
-        rotation="00:00",
-        retention="1 week",
+        'logs/error.log',
+        rotation='00:00',
+        retention='1 week',
         diagnose=False,
-        level="ERROR",
+        level='ERROR',
         format=default_format,
-        encoding="utf-8",
+        encoding='utf-8',
     )
     logger.add(
-        "logs/info.log",
-        rotation="00:00",
-        retention="1 week",
+        'logs/info.log',
+        rotation='00:00',
+        retention='1 week',
         diagnose=False,
-        level="INFO",
+        level='INFO',
         format=default_format,
-        encoding="utf-8",
+        encoding='utf-8',
     )
     logger.add(
-        "logs/warning.log",
-        rotation="00:00",
-        retention="1 week",
+        'logs/warning.log',
+        rotation='00:00',
+        retention='1 week',
         diagnose=False,
-        level="WARNING",
+        level='WARNING',
         format=default_format,
-        encoding="utf-8",
+        encoding='utf-8',
     )
     if driver.config.use_plugin_go_cqhttp:
-        nonebot.load_plugin("nonebot_plugin_gocqhttp")
-    nonebot.load_from_toml("pyproject.toml")
+        nonebot.load_plugin('nonebot_plugin_gocqhttp')
+    nonebot.load_from_toml('pyproject.toml')
     # ...
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # logger.warning("Always use `nb run` to start the bot instead of manually running!")
-    nonebot.load_plugin("nonebot_plugin_reboot")
-    nonebot.run(app="__mp_main__:app")
+    nonebot.load_plugin('nonebot_plugin_reboot')
+    nonebot.run(app='__mp_main__:app')
